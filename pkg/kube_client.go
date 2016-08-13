7f747898b03b6619f81bd555805a23bcae16702a
Mikhail Kokho <mikhail@movio.co>
2016-08-14 09:51:17 +1200

    Add client to kube-apiserver

---
+++ pkg/kube_client.go
@@ -0,0 +1,102 @@
+package pkg
+
+import (
+	"net/url"
+	"net/http"
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
+
+type KubeClient struct {
+	client   *http.Client
+	BaseURL *url.URL
+}
+
+func NewKubeClient() *KubeClient {
+	c := &KubeClient{client: http.DefaultClient}
+	return c
+}
+
+func (kc *KubeClient) getPods() ([]Pod, error) {
+	req, err := kc.newRequest("GET", "api/v1/pods", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	podList := new(PodList)
+	err = kc.Do(req, podList)
+	if err != nil {
+		return nil, err
+	}
+
+	return podList.Items, nil
+}
+
+func (kc *KubeClient) getServices() ([]Service, error) {
+	req, err := kc.newRequest("GET", "api/v1/services", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	svcList := new(ServiceList)
+	err = kc.Do(req, svcList)
+	if err != nil {
+		return nil, err
+	}
+
+	return svcList.Items, nil
+}
+
+func (kc *KubeClient) newRequest(method string, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	u := kc.BaseURL.ResolveReference(rel)
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, nil
+}
+
+func (c *KubeClient) Do(req *http.Request, v interface{}) error {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		// Drain up to 512 bytes and close the body to let the Transport reuse the connection
+		io.CopyN(ioutil.Discard, resp.Body, 512)
+		resp.Body.Close()
+	}()
+
+	if v != nil {
+		//io.Copy(os.Stdout, resp.Body)
+		err = json.NewDecoder(resp.Body).Decode(v)
+		if err == io.EOF {
+			err = nil // ignore EOF errors caused by empty response body
+		}
+	}
+
+	return err
+}
+
---
+++ pkg/kube_client_test.go
@@ -0,0 +1,92 @@
+package pkg
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"net/url"
+	"fmt"
+	"reflect"
+)
+
+var (
+	mux *http.ServeMux
+	server *httptest.Server
+	client *KubeClient
+
+)
+
+func setup() {
+	// test server
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	url, _ := url.Parse(server.URL)
+	client = NewKubeClient()
+	client.BaseURL = url
+}
+
+// teardown closes the test HTTP server.
+func teardown() {
+	server.Close()
+}
+
+func TestFetchPods(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v1/pods", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `
+			{
+				"items": [
+					{ "metadata": { "name": "pod1" } },
+					{ "metadata": { "name": "pod2" } }
+				]
+			}`)
+		},
+	)
+
+	pods, err := client.getPods()
+	if err != nil {
+		t.Errorf("getPods returned error: %v", err)
+	}
+
+	want := []Pod{
+		Pod { ObjectMeta: ObjectMeta{Name: "pod1"} },
+		Pod { ObjectMeta: ObjectMeta{Name: "pod2"} },
+	}
+
+	if !reflect.DeepEqual(pods, want) {
+		t.Errorf("getPods returned %+v, want %+v", pods, want)
+	}
+}
+
+func TestFetchServices(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v1/services", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `
+			{
+				"items": [
+					{ "metadata": { "name": "svc1" } },
+					{ "metadata": { "name": "svc2" } }
+				]
+			}`)
+		},
+	)
+
+	svc, err := client.getServices()
+	if err != nil {
+		t.Errorf("getServices returned error: %v", err)
+	}
+
+	want := []Service{
+		Service { ObjectMeta: ObjectMeta{Name: "svc1"} },
+		Service { ObjectMeta: ObjectMeta{Name: "svc2"} },
+	}
+
+	if !reflect.DeepEqual(svc, want) {
+		t.Errorf("getPods returned %+v, want %+v", svc, want)
+	}
+}
