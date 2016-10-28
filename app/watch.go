847a5a9dbcabd961e64a0b9d2bc147d9b24f4133
Mikhail Kokho <mikhail@movio.co>
2016-10-28 22:43:10 +1300

    remove typed loopWatch methods

--- app/watch.go
+++ app/watch.go
@@ -51,9 +51,7 @@ func RunWatch(f Factory, cmd *cobra.Command, args []string) {
 		kc := f.KubeClient(url)
 		log.Infof("Created kube client for %s", args[i])
 
-		loopWatchPods(c, kc)
-		loopWatchServices(c, kc)
-		loopWatchDeployments(c, kc)
+		loopWatchObjects(c, kc, "pod")
 	}
 
 	log.Infof("Kube Mirror is listening on %s", bind)
@@ -95,105 +93,6 @@ func loopWatchObjects(c *MrrCache, kc KubeClient, kind string) {
 				case Added, Modified:
 					c.updateKubeObject(kc.Server(), *e.Object)
 				}
-			}
-		}
-	}
-
-	go watch()
-	go update()
-}
-
-func loopWatchPods(c *MrrCache, kc KubeClient) {
-	events := make(chan *ObjectEvent)
-
-	watch := func() {
-		for {
-			log.Infof("Started to watch pods for %s", kc.BaseURL().String())
-			err := kc.WatchObjects("pod", events)
-			if err != nil {
-				log.Infof("Disruption while watching pods: %s", err)
-			}
-		}
-	}
-
-	update := func() {
-		for {
-			select {
-			case e := <-events:
-				log.Infof("Received event [%s] for pod [%s]", e.Type, e.Object.Name)
-				switch e.Type {
-				case Deleted:
-					c.deleteKubeObject(kc.Server(), *e.Object)
-				case Added, Modified:
-					c.updateKubeObject(kc.Server(), *e.Object)
-				}
-				//log.WithField("pods", c.objects).Debugf("Cached objects")
-			}
-		}
-	}
-
-	go watch()
-	go update()
-}
-
-func loopWatchServices(c *MrrCache, kc KubeClient) {
-	events := make(chan *ServiceEvent)
-
-	watch := func() {
-		for {
-			log.Infof("Started to watch services for %s", kc.BaseURL().String())
-			err := kc.WatchServices(events)
-			if err != nil {
-				log.Infof("Disruption while watching services: %s", err)
-			}
-		}
-	}
-
-	update := func() {
-		for {
-			select {
-			case e := <-events:
-				log.Infof("Received event [%s] for service [%s]", e.Type, e.Service.Name)
-				switch e.Type {
-				case Deleted:
-					c.removeService(e.Service)
-				case Added, Modified:
-					c.updateService(e.Service)
-				}
-				log.WithField("services", c.services).Debugf("Cached services")
-			}
-		}
-	}
-
-	go watch()
-	go update()
-}
-
-func loopWatchDeployments(c *MrrCache, kc KubeClient) {
-	events := make(chan *DeploymentEvent)
-
-	watch := func() {
-		for {
-			log.Infof("Started to watch deployments for %s", kc.BaseURL().String())
-			err := kc.WatchDeployments(events)
-			if err != nil {
-				log.Infof("Disruption while watching services: %s", err)
-			}
-		}
-	}
-
-	update := func() {
-		for {
-			select {
-			case e := <-events:
-				log.Infof("Received event [%s] for deployment [%s]", e.Type, e.Deployment.Name)
-				switch e.Type {
-				case Deleted:
-					c.removeDeployment(e.Deployment)
-				case Added, Modified:
-					c.updateDeployment(e.Deployment)
-				}
-				log.WithField("deployments", c.deployments).Debugf("Cached deployments")
 			}
 		}
 	}
--- app/watch_test.go
+++ app/watch_test.go
@@ -61,23 +61,11 @@ func TestRunWatch(t *testing.T) {
 	time.Sleep(10 * time.Millisecond)
 
 	for s, kc := range f.kubeClients {
-		for _, w := range []string{"WatchObjects", "WatchServices", "WatchDeployments"} {
+		for _, w := range []string{"WatchObjects"} {
 			if kc.hits[w] < 1 {
 				t.Errorf("Not enough %s requests for server %s", w, s)
 			}
 		}
-	}
-
-	if c.pods == nil {
-		t.Errorf("Pods in the cache has not been updated")
-	}
-
-	if c.services == nil {
-		t.Errorf("Services in the cache has not been updated")
-	}
-
-	if c.deployments == nil {
-		t.Errorf("Deployments in the cache has not been updated")
 	}
 }
 
@@ -93,7 +81,6 @@ func TestLoopWatchObjectsFailure(t *testing.T) {
 		t.Errorf("Not enough WatchObjects calls")
 	}
 }
-
 
 func TestLoopWatchObjects(t *testing.T) {
 	c := NewMrrCache()
@@ -116,135 +103,5 @@ func TestLoopWatchObjects(t *testing.T) {
 	actual := c.objects[kc.Server()]
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Cache version %+v is not equal to expected %+v", actual, expected)
-	}
-}
-
-func TestLoopWatchPodsFailure(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.errors["WatchObjects"] = errors.New("Test Error")
-
-	loopWatchPods(c, kc)
-
-	time.Sleep(10 * time.Millisecond)
-	if kc.hits["WatchObjects"] < 2 {
-		t.Errorf("Not enough WatchObjects calls")
-	}
-}
-
-func TestLoopWatchPods(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.objectEvents = []*ObjectEvent{
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "a"}}},
-		{Deleted, &KubeObject{ObjectMeta: ObjectMeta{Name: "a"}}},
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "pod1"}}},
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "pod0"}}},
-		{Modified, &KubeObject{ObjectMeta: ObjectMeta{Name: "pod1", ResourceVersion: "v2"}}},
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "z"}}},
-		{Deleted, &KubeObject{ObjectMeta: ObjectMeta{Name: "z"}}},
-	}
-
-	loopWatchPods(c, kc)
-	time.Sleep(10 * time.Millisecond)
-
-	//order is matter in slice
-	expected := []KubeObject{*kc.objectEvents[4].Object, *kc.objectEvents[3].Object}
-	actual := c.objects[kc.Server()]
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Cache version %+v is not equal to expected %+v", actual, expected)
-	}
-}
-
-func TestLoopWatchPodsWithNamespaces(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.objectEvents = []*ObjectEvent{
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "a", Namespace: "y1"}}},
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "a", Namespace: "y2"}}},
-		{Added, &KubeObject{ObjectMeta: ObjectMeta{Name: "a", Namespace: "y3"}}},
-		{Deleted, &KubeObject{ObjectMeta: ObjectMeta{Name: "a", Namespace: "y3"}}},
-	}
-
-	loopWatchPods(c, kc)
-	time.Sleep(10 * time.Millisecond)
-
-	//order is matter is slice
-	expected := []KubeObject{*kc.objectEvents[0].Object, *kc.objectEvents[1].Object}
-	actual := c.objects[kc.Server()]
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Cache version %+v is not equal to expected %+v", actual, expected)
-	}
-}
-
-func TestLoopWatchServicesFailure(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.errors["WatchServices"] = errors.New("Test Error")
-
-	loopWatchServices(c, kc)
-
-	time.Sleep(10 * time.Millisecond)
-	if kc.hits["WatchServices"] < 2 {
-		t.Errorf("Not enough WatchServices calls")
-	}
-}
-
-func TestLoopWatchServices(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.serviceEvents = []*ServiceEvent{
-		{Added, &Service{ObjectMeta: ObjectMeta{Name: "service0"}}},
-		{Added, &Service{ObjectMeta: ObjectMeta{Name: "service1"}}},
-		{Modified, &Service{ObjectMeta: ObjectMeta{Name: "service1", ResourceVersion: "v2"}}},
-		{Deleted, &Service{ObjectMeta: ObjectMeta{Name: "service0"}}},
-	}
-
-	loopWatchServices(c, kc)
-	time.Sleep(10 * time.Millisecond)
-
-	expected := *kc.serviceEvents[2].Service
-	if !reflect.DeepEqual(*c.services["service1"], expected) {
-		t.Errorf("Cache version %v is not equal to expected %v", c.services["service1"], expected)
-	}
-
-	if _, ok := c.services["service0"]; ok {
-		t.Errorf("Pod [%s] should have been deleted", "service0")
-	}
-}
-
-func TestLoopWatchDeploymentsFailure(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.errors["WatchDeployments"] = errors.New("Test Error")
-
-	loopWatchDeployments(c, kc)
-
-	time.Sleep(10 * time.Millisecond)
-	if kc.hits["WatchDeployments"] < 2 {
-		t.Errorf("Not enough WatchDeployments calls")
-	}
-}
-
-func TestLoopWatchDeployments(t *testing.T) {
-	c := NewMrrCache()
-	kc := NewTestKubeClient()
-	kc.deploymentEvents = []*DeploymentEvent{
-		{Added, &Deployment{ObjectMeta: ObjectMeta{Name: "deployment0"}}},
-		{Added, &Deployment{ObjectMeta: ObjectMeta{Name: "deployment1"}}},
-		{Modified, &Deployment{ObjectMeta: ObjectMeta{Name: "deployment1", ResourceVersion: "v2"}}},
-		{Deleted, &Deployment{ObjectMeta: ObjectMeta{Name: "deployment0"}}},
-	}
-
-	loopWatchDeployments(c, kc)
-	time.Sleep(10 * time.Millisecond)
-
-	expected := *kc.deploymentEvents[2].Deployment
-	if !reflect.DeepEqual(*c.deployments["deployment1"], expected) {
-		t.Errorf("Cache version %v is not equal to expected %v", c.deployments["deployment1"], expected)
-	}
-
-	if _, ok := c.deployments["deployment0"]; ok {
-		t.Errorf("Pod [%s] should have been deleted", "deployment0")
 	}
 }
