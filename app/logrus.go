c01f4b2147917b1ad4303cbf26cc74ecbb3aa83d
Mikhail Kokho <mikhail@movio.co>
2016-11-06 23:55:00 +1300

    write logs to stdout

--- app/logrus.go
+++ app/logrus.go
@@ -1,8 +1,12 @@
 package app
 
-import log "github.com/Sirupsen/logrus"
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+)
 
 func init() {
+	log.SetOutput(os.Stdout)
 	log.SetLevel(log.InfoLevel)
 	log.SetFormatter(&log.JSONFormatter{TimestampFormat: "2006-01-02T15:04:05.000"})
 }
